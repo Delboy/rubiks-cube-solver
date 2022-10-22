2ac451f5971d1db6dfe7e8e4411bf7267cd1039d
Derek de Goey <derek.degoey@hotmail.co.uk>
2022-10-22 12:36:56 +0100

    added cube face sections

--- src/components/Cube/CubeFace.js
+++ src/components/Cube/CubeFace.js
@@ -4,7 +4,17 @@ const CubeFace = (props) => {
     const cubeFaceClassName = (classes[props.face])
 
   return (
-    <div className={`${classes.cubeFace} ${cubeFaceClassName}`}>{props.face}</div>
+    <div className={`${classes.cubeFace} ${cubeFaceClassName}`}>
+        <div>TL</div>
+        <div>TM</div>
+        <div>TR</div>
+        <div>CL</div>
+        <div>C</div>
+        <div>CR</div>
+        <div>BL</div>
+        <div>BM</div>
+        <div>BR</div>
+    </div>
   );
 };
 
