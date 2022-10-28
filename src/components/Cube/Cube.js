c0ab35229041e898a283fbfe1db9c3724fe2e985
Derek Warren <derek.degoey@hotmail.co.uk>
2022-10-28 13:17:21 +0100

    added useEffects that set back and right side face

--- src/components/Cube/Cube.js
+++ src/components/Cube/Cube.js
@@ -5,7 +5,8 @@ import CubeFace from "./CubeFace";
 const Cube = (props) => {
   const [disableButton, setDisableButton] = useState(false)
   const [currentFace, setCurrentFace] = useState("blue");
-  // const [isBlueOnSide, setIsBlueOnSide] = useState(false)
+  const [rightSideFace, setRightSideFace] = useState('red')
+  const [backFace, setBackFace] = useState('green')
   const [yAxis, setYaxis] = useState(0);
   const [xAxis, setXaxis] = useState(0);
   const [zAxis, setZaxis] = useState(0);
@@ -51,16 +52,70 @@ const Cube = (props) => {
 
   }, [yAxis, xAxis, zAxis]);
 
-  // Finds where the blue side
+  // Sets the Front face
+  const updateFaceHandler = (face) => {
+    setCurrentFace(face);
+  };
+
+  // sets the Back face 
   useEffect(() => {
+    if(currentFace === 'blue'){
+      setBackFace('green')
+    }
+    if(currentFace === 'green'){
+      setBackFace('blue')
+    }
+    if(currentFace === 'red'){
+      setBackFace('orange')
+    }
+    if(currentFace === 'orange'){
+      setBackFace('red')
+    }
+    if(currentFace === 'white'){
+      setBackFace('yellow')
+    }
+    if(currentFace === 'yellow'){
+      setBackFace('white')
+    }
+  }, [currentFace])
 
+  // Sets the Right side face
+  const updateSideHandler = (side) => {
+    console.log('face: ', side.face, 'coord: ', side.coord)
+    
+    let newArray = coordArray
+    
+    newArray.map(obj => {
+      if (obj.face === side.face) {
+        obj.coord = side.coord
+        return
+      } else {
+        return obj
+      }
+    })
 
-  })
+    setCoordArray(newArray)
+  }
+  
+  useEffect(() => {
+    let newArray = coordArray
+    newArray.sort(function(a, b){return b.coord - a.coord});
+    if(newArray[0].coord === newArray[1].coord){
+      if(newArray[0].face === currentFace || newArray[0].face === backFace){
+        setRightSideFace(newArray[1].face)
+      } else {
+        setRightSideFace(newArray[0].face)
+      }
+    } else {
+      setRightSideFace(newArray[0].face)
+    }
+  },[coordArray, currentFace, backFace])
 
   let rotation = {
     transform: `translateZ(-100px) rotateY(${yAxis}deg) rotateX(${xAxis}deg) rotateZ(${zAxis}deg)`,
   };
 
+  // Handles Button Presses
   const buttonHandler = (e) => {
     setDisableButton(true)
 
@@ -88,27 +143,10 @@ const Cube = (props) => {
     }
   };
 
-  const updateFaceHandler = (face) => {
-    setCurrentFace(face);
-  };
-
-  const updateSideHandler = (side) => {
-    let newArray = coordArray
-
-    newArray.map(obj => {
-      if (obj.face === side.face) {
-        obj.coord = side.coord
-        return
-      } else {
-        return obj
-      }
-    })
-
-    setCoordArray(newArray)
-  }
-
   const consoleHandler = () => {
     console.log(coordArray)
+    console.log('current face: ', currentFace)
+    console.log('right side face: ', rightSideFace)
   }
 
   return (
