59fb07474124240d54e794f985c70f856b893d30
Derek de Goey <derek.degoey@hotmail.co.uk>
2022-12-27 13:03:21 +0000

    Added function in firstLayerSolver to solve white edges in top row, added function to Solvers to handle multistage commands and errors in moves made by user

--- src/components/Solvers/FirstLayerSolver.jsx
+++ src/components/Solvers/FirstLayerSolver.jsx
@@ -1,13 +1,10 @@
 import { useCallback, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { guideActions } from "../../orientation";
+import { useSelector } from "react-redux";
 
 const FirstLayerSolver = (props) => {
   const daisySolved = useSelector((state) => state.guide.daisySolved);
   const whiteCrossSolved = useSelector((state) => state.guide.whiteCrossSolved);
   const firstLayerSolved = useSelector((state) => state.guide.firstLayerSolved);
-
-  const dispatch = useDispatch();
 
   const blueLeftCorner = useSelector((state) => state.faces.segmentState.btl);
   const blueRightCorner = useSelector((state) => state.faces.segmentState.btr);
@@ -20,94 +17,187 @@ const FirstLayerSolver = (props) => {
   const redLeftCorner = useSelector((state) => state.faces.segmentState.rtl);
   const redRightCorner = useSelector((state) => state.faces.segmentState.rtr);
 
+  const moveCounter = useSelector((state) => state.faces.moveCounter);
+
   const topLayerFacingOut = useCallback(() => {
-    console.log('top face layer running')
     const topCorners = [
       {
-        face: 'blue',
+        face: "blue",
         edgeOne: blueLeftCorner,
         edgeTwo: blueRightCorner,
         edgeOnePair: orangeRightCorner,
         edgeTwoPair: redLeftCorner,
       },
       {
-        face: 'orange',
+        face: "orange",
         edgeOne: orangeLeftCorner,
         edgeTwo: orangeRightCorner,
         edgeOnePair: greenRightCorner,
         edgeTwoPair: blueLeftCorner,
       },
       {
-        face: 'green',
+        face: "green",
         edgeOne: greenLeftCorner,
         edgeTwo: greenRightCorner,
         edgeOnePair: redRightCorner,
         edgeTwoPair: orangeLeftCorner,
       },
       {
-        face: 'red',
+        face: "red",
         edgeOne: redLeftCorner,
         edgeTwo: redRightCorner,
         edgeOnePair: blueRightCorner,
         edgeTwoPair: greenLeftCorner,
       },
-      
     ];
 
-    let faceOrder = ['blue', 'orange', 'green', 'red']
-    topCorners.forEach((corner) => {
-        let faceWithWhiteIndex = faceOrder.indexOf(corner.face)
-      if (corner.edgeOne === "white") {
-        let faceItsOn = faceWithWhiteIndex + 1
-        if(faceItsOn === 4){
-            faceItsOn = 0
-        }
-        
-        let indexOfWhereItNeedsToBe = faceOrder.indexOf(corner.edgeOnePair)
+    const faceOrder = ["blue", "red", "green", "orange"];
 
-        let ammoutOfMoves = faceItsOn - indexOfWhereItNeedsToBe
-        if(ammoutOfMoves === -3){
-            ammoutOfMoves = 1
+    topCorners.forEach((corner) => {
+      // Index of what face has a white edge piece
+      let faceWithWhiteIndex = faceOrder.indexOf(corner.face);
+
+      // Works out what color face is to the right of the paired color,
+      // as this is the face that needs to rotate when doing the alogrithm
+      let edgeOnePairIndex = faceOrder.indexOf(corner.edgeOnePair);
+      let rightOfEdgePairOneIndex = edgeOnePairIndex + 1;
+      if (rightOfEdgePairOneIndex === 4) {
+        rightOfEdgePairOneIndex = 0;
+      }
+      let rightOfEdgePairOne = faceOrder[rightOfEdgePairOneIndex];
+
+      // Works out what color face is to the left of the paired color,
+      // as this is the face that needs to rotate when doing the alogrithm
+      let edgeTwoPairIndex = faceOrder.indexOf(corner.edgeTwoPair);
+      let leftOfEdgePairTwoIndex = edgeTwoPairIndex - 1;
+      if (leftOfEdgePairTwoIndex === -1) {
+        leftOfEdgePairTwoIndex = 3;
+      }
+      let leftOfEdgePairTwo = faceOrder[leftOfEdgePairTwoIndex];
+
+      // Variables
+      let faceItsOn;
+      let indexOfWhereItNeedsToBe;
+      let move = "clockwise";
+      let algoDirection = "y";
+
+      let colorCornerPos;
+      let whiteCornerPos;
+      let cornerEdgePair;
+      let algoString;
+      let case0Algo;
+      let case1Algo;
+      let case2Algo;
+
+      // Works out what face the colored side of the corner with a white edge is on
+      if (corner.edgeOne === "white") {
+        faceItsOn = faceWithWhiteIndex - 1;
+        if (faceItsOn === -1) {
+          faceItsOn = 3;
         }
-        let move = "anticlockwise"
-        if(ammoutOfMoves < 0){
-            move = "clockwise"
-            ammoutOfMoves = ammoutOfMoves * -1
-        }
-        
-        console.log('left',corner.face, move, ammoutOfMoves) 
-        dispatch(guideActions.setCommand(
-            `The ${corner.face} face has a white edge in the top left corner, so match up it's adjacent color ${corner.edgeOnePair} to the same color center by turning the top ${move} ${ammoutOfMoves} times!`
-        ))  
-        
-        // send commands off to multi stage solver function
-        
+        indexOfWhereItNeedsToBe = faceOrder.indexOf(corner.edgeOnePair);
       }
       if (corner.edgeTwo === "white") {
-        let faceItsOn = faceWithWhiteIndex - 1
-        if(faceItsOn === -1){
-            faceItsOn = 3
-        } 
-        
-        let indexOfWhereItNeedsToBe = faceOrder.indexOf(corner.edgeTwoPair) 
-        let ammoutOfMoves = faceItsOn - indexOfWhereItNeedsToBe
-        
-        if(ammoutOfMoves === -3){
-            ammoutOfMoves = 1
+        faceItsOn = faceWithWhiteIndex + 1;
+        if (faceItsOn === 4) {
+          faceItsOn = 0;
         }
-        let move = "anticlockwise"
-        if(ammoutOfMoves < 0){
-            move = "clockwise"
-            ammoutOfMoves = ammoutOfMoves * -1
-        }
-        
-        console.log('right',corner.face, move, ammoutOfMoves)   
-        dispatch(guideActions.setCommand(
-            `The ${corner.face} face has a white edge in the top right corner, so match up it's adjacent color ${corner.edgeTwoPair} to the same color center by turning the top ${move} ${ammoutOfMoves} times!`
-        ))   
+        indexOfWhereItNeedsToBe = faceOrder.indexOf(corner.edgeTwoPair);
+      }
 
-        // send commands off to multi stage solver function
-        
+      // Works out how many moves are needed to get the colored side of the corner to align with the same colored face
+      let ammoutOfMoves = faceItsOn - indexOfWhereItNeedsToBe;
+      if (ammoutOfMoves === -3) {
+        ammoutOfMoves = -1;
+      }
+      if (ammoutOfMoves < 0) {
+        move = "anticlockwise";
+        algoDirection = "yp";
+        ammoutOfMoves = ammoutOfMoves * -1;
+      }
+
+      // Updates variables depending on what corner has the white edge
+      if (corner.edgeOne === "white") {
+        colorCornerPos = "right";
+        whiteCornerPos = "left";
+        cornerEdgePair = corner.edgeOnePair;
+        algoString = ["R", "U", "R'"];
+        case0Algo =
+          [`${corner.face.charAt(0)}`, "y", `${corner.face.charAt(0)}p`];
+        case1Algo =
+          [`${algoDirection}`,
+          `${rightOfEdgePairOne.charAt(0)}`,
+          "y",
+          `${rightOfEdgePairOne.charAt(0)}p`];
+        case2Algo =
+          [`y`,
+          `y`,
+          `${rightOfEdgePairOne.charAt(0)}`,
+          "y",
+          `${rightOfEdgePairOne.charAt(0)}p`];
+      }
+      if (corner.edgeTwo === "white") {
+        colorCornerPos = "left";
+        whiteCornerPos = "right";
+        cornerEdgePair = corner.edgeTwoPair;
+        algoString = ["L'", "U'", "L"];
+        case0Algo = [`${corner.face.charAt(0)}p`, "yp", `${corner.face.charAt(0)}`]
+        case1Algo =
+          [`${algoDirection}`,
+          `${leftOfEdgePairTwo.charAt(0)}p`,
+          "yp",
+          `${leftOfEdgePairTwo.charAt(0)}`]
+        case2Algo =
+          [`y`,
+          `y`,
+          `${leftOfEdgePairTwo.charAt(0)}p`,
+          "yp",
+          `${leftOfEdgePairTwo.charAt(0)}`];
+      }
+
+      // Runs commands depending on how many moves needed
+      switch (ammoutOfMoves) {
+        case 0:
+          props.setValuesForMultiStageCommand(
+            true,
+            moveCounter,
+            [
+              `The top ${colorCornerPos} corner piece on the ${cornerEdgePair} face matches the face's center piece and has a white side, so facing the ${cornerEdgePair} face peform the algorithm ${algoString[0]}, ${algoString[1]}, ${algoString[2]}`,
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[1]}`,
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[2]}`,
+            ],
+            case0Algo
+          );
+          break;
+        case 1:
+          props.setValuesForMultiStageCommand(
+            true,
+            moveCounter,
+            [
+              `The ${corner.face} face has a white edge in the top ${whiteCornerPos} corner, so match up it's adjacent color ${cornerEdgePair} to the same color center by turning the top ${move} one time!`,
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[0]}`,
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[1]}`,
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[2]}`,
+            ],
+            case1Algo
+          );
+          break;
+        case 2:
+          props.setValuesForMultiStageCommand(
+            true,
+            moveCounter,
+            [
+              `The ${corner.face} face has a white edge in the top ${whiteCornerPos} corner, so match up it's adjacent color ${cornerEdgePair} to the same color center by turning the top either direction twice!`,
+              "Keep turning",
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[0]}`,
+              `Now. facing the ${cornerEdgePair} face, perform ${algoString[1]}`,
+              `Now facing the ${cornerEdgePair} face, perform ${algoString[2]}`,
+            ],
+            case2Algo
+          );
+          break;
+        default:
+          break;
       }
     });
   }, [
@@ -119,28 +209,23 @@ const FirstLayerSolver = (props) => {
     orangeRightCorner,
     redLeftCorner,
     redRightCorner,
-    dispatch
-   
+    moveCounter,
+    props,
   ]);
 
   const firstLayerSolver = useCallback(() => {
-    
-    if (!props.twoStageCommand) {
-      console.log('no two stage')
+    if (!props.multiStageCommand) {
       // checks if white corners facing out in top layer
       topLayerFacingOut();
     }
-    if (props.twoStageCommand) {
-        console.log('two stage')
-      // checks if any commands had two parts
-      props.checkTwoStageCommand();
+    if (props.multiStageCommand) {
+      props.multiStageCommandSetter();
     }
   }, [topLayerFacingOut, props]);
 
   // Runs the solver if white cross not solved
   useEffect(() => {
     if (daisySolved && whiteCrossSolved && !firstLayerSolved) {
-      console.log("useEffect for FLS");
       firstLayerSolver();
     }
   }, [daisySolved, whiteCrossSolved, firstLayerSolved, firstLayerSolver]);
--- src/components/Solvers/Solvers.jsx
+++ src/components/Solvers/Solvers.jsx
@@ -1,5 +1,7 @@
+// start
+
 import { useDispatch, useSelector } from "react-redux";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { guideActions } from "../../orientation";
 
 import DaisySolver from "./DaisySolver";
@@ -17,6 +19,23 @@ const Solvers = (props) => {
   const [twoStageCommand, setTwoStageCommand] = useState(false);
   // Second Command string
   const [secondCommand, setSecondCommand] = useState(null);
+
+  // multistage command
+  const [multiStageCommand, setMultiStageCommand] = useState(false);
+  // multstage counter
+  const [multiStageCounter, setMultiStageCounter] = useState(0);
+  // messages array
+  const [messagesArray, setMessagesArray] = useState([]);
+
+  // algo array
+  const [algo, setAlgo] = useState([]);
+  // last move made
+  const lastMove = useSelector((state) => state.faces.lastMove);
+
+  // error
+  const [error, setError] = useState(false)
+  const [errorAlgo, setErrorAlgo] = useState([])
+  const [errorMessages, setErrorMessages] = useState([])
 
   const setValuesForTwoStageCommand = (
     twoStageCommandBool,
@@ -38,6 +57,76 @@ const Solvers = (props) => {
     }
   }, [moveCounter, savedMoveCount, secondCommand, dispatch]);
 
+  // Sets values for multistage function to use
+  const setValuesForMultiStageCommand = (
+    multiStageCommandBool,
+    currentMoveCount,
+    messages,
+    algorithm
+  ) => {
+    setSavedMoveCount(currentMoveCount);
+    setMessagesArray(messages);
+    setAlgo(algorithm);
+
+    dispatch(guideActions.setCommand(messages[0]));
+
+    setMultiStageCommand(multiStageCommandBool);
+
+    multiStageCommandSetter();
+  };
+
+  const multiStageCommandSetter = useCallback(() => {
+    if (moveCounter === savedMoveCount + (multiStageCounter + 1)) {
+      // If the move made is the move expected
+      if (lastMove === algo[multiStageCounter]) {
+        dispatch(guideActions.setCommand(messagesArray[multiStageCounter + 1]));
+        setMultiStageCounter((prevState) => (prevState += 1));
+        if (moveCounter === savedMoveCount + messagesArray.length) {
+          setMultiStageCounter(0);
+          setMultiStageCommand(false);
+        }
+      }
+      // If the move made is not what was expected
+      if (lastMove !== algo[multiStageCounter]) {
+        
+        let wrongMove = lastMove;
+        let reverseOfWrongMove;
+        if (wrongMove.includes("p")) {
+          reverseOfWrongMove = wrongMove.charAt(0);
+        } else {
+          reverseOfWrongMove = wrongMove + "p";
+        }
+        const currentArray = algo;
+        const newAlgo = currentArray.slice(multiStageCounter);
+        newAlgo.unshift(reverseOfWrongMove);
+        const firstMessage = `Oh no, it looks like you performed the wrong move! Reverse this move by performing ${reverseOfWrongMove}`;
+        const currentMessages = messagesArray;
+        let newMessages;
+       
+        newMessages = currentMessages.slice(multiStageCounter);
+        
+        newMessages.unshift(firstMessage);
+
+        setErrorMessages(newMessages)
+        setErrorAlgo(newAlgo)
+        setError(true)
+        
+      }
+    } 
+  },[algo, moveCounter, lastMove, dispatch, messagesArray, multiStageCounter, savedMoveCount ]);
+
+  useEffect(() => {
+    if(error){
+      setAlgo(errorAlgo)
+      setMessagesArray(errorMessages)
+      setSavedMoveCount(moveCounter)
+      dispatch(guideActions.setCommand(errorMessages[0]))
+      setError(false)
+      setMultiStageCounter(0)
+      multiStageCommandSetter()
+    }
+  },[error,errorAlgo, errorMessages, moveCounter, dispatch, multiStageCommandSetter])
+
   return (
     <>
       <DaisySolver
@@ -45,15 +134,18 @@ const Solvers = (props) => {
         setValuesForTwoStageCommand={setValuesForTwoStageCommand}
         twoStageCommand={twoStageCommand}
       />
-      <WhiteCrossSolver 
+      <WhiteCrossSolver
         checkTwoStageCommand={checkTwoStageCommand}
         setValuesForTwoStageCommand={setValuesForTwoStageCommand}
         twoStageCommand={twoStageCommand}
       />
-      <FirstLayerSolver 
+      <FirstLayerSolver
         checkTwoStageCommand={checkTwoStageCommand}
         setValuesForTwoStageCommand={setValuesForTwoStageCommand}
         twoStageCommand={twoStageCommand}
+        multiStageCommandSetter={multiStageCommandSetter}
+        setValuesForMultiStageCommand={setValuesForMultiStageCommand}
+        multiStageCommand={multiStageCommand}
       />
     </>
   );
