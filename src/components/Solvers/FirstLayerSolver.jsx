67901a3ac2ab93d5f06e6150098f13682d54bb75
Derek de Goey <derek.degoey@hotmail.co.uk>
2022-12-28 13:20:19 +0000

    Added function to firstLayerSolver that checks for white edges in bottom row and moves them to top

--- src/components/Solvers/FirstLayerSolver.jsx
+++ src/components/Solvers/FirstLayerSolver.jsx
@@ -6,48 +6,89 @@ const FirstLayerSolver = (props) => {
   const whiteCrossSolved = useSelector((state) => state.guide.whiteCrossSolved);
   const firstLayerSolved = useSelector((state) => state.guide.firstLayerSolved);
 
-  const blueLeftCorner = useSelector((state) => state.faces.segmentState.btl);
-  const blueRightCorner = useSelector((state) => state.faces.segmentState.btr);
-  const orangeLeftCorner = useSelector((state) => state.faces.segmentState.otl);
-  const orangeRightCorner = useSelector(
+  // Top Layer Corners
+  const blueTopLeftCorner = useSelector(
+    (state) => state.faces.segmentState.btl
+  );
+  const blueTopRightCorner = useSelector(
+    (state) => state.faces.segmentState.btr
+  );
+  const orangeTopLeftCorner = useSelector(
+    (state) => state.faces.segmentState.otl
+  );
+  const orangeTopRightCorner = useSelector(
     (state) => state.faces.segmentState.otr
   );
-  const greenLeftCorner = useSelector((state) => state.faces.segmentState.gtl);
-  const greenRightCorner = useSelector((state) => state.faces.segmentState.gtr);
-  const redLeftCorner = useSelector((state) => state.faces.segmentState.rtl);
-  const redRightCorner = useSelector((state) => state.faces.segmentState.rtr);
+  const greenTopLeftCorner = useSelector(
+    (state) => state.faces.segmentState.gtl
+  );
+  const greenTopRightCorner = useSelector(
+    (state) => state.faces.segmentState.gtr
+  );
+  const redTopLeftCorner = useSelector((state) => state.faces.segmentState.rtl);
+  const redTopRightCorner = useSelector(
+    (state) => state.faces.segmentState.rtr
+  );
 
+  // Bottom Layer Corners
+  const blueBottomLeftCorner = useSelector(
+    (state) => state.faces.segmentState.bbl
+  );
+  const blueBottomRightCorner = useSelector(
+    (state) => state.faces.segmentState.bbr
+  );
+  const orangeBottomLeftCorner = useSelector(
+    (state) => state.faces.segmentState.obl
+  );
+  const orangeBottomRightCorner = useSelector(
+    (state) => state.faces.segmentState.obr
+  );
+  const greenBottomLeftCorner = useSelector(
+    (state) => state.faces.segmentState.gbl
+  );
+  const greenBottomRightCorner = useSelector(
+    (state) => state.faces.segmentState.gbr
+  );
+  const redBottomLeftCorner = useSelector(
+    (state) => state.faces.segmentState.rbl
+  );
+  const redBottomRightCorner = useSelector(
+    (state) => state.faces.segmentState.rbr
+  );
+
+  // Move counter
   const moveCounter = useSelector((state) => state.faces.moveCounter);
 
+  // Finds corners with white edges in top row and returns algorithm to solve
   const topLayerFacingOut = useCallback(() => {
     const topCorners = [
       {
         face: "blue",
-        edgeOne: blueLeftCorner,
-        edgeTwo: blueRightCorner,
-        edgeOnePair: orangeRightCorner,
-        edgeTwoPair: redLeftCorner,
+        edgeOne: blueTopLeftCorner,
+        edgeTwo: blueTopRightCorner,
+        edgeOnePair: orangeTopRightCorner,
+        edgeTwoPair: redTopLeftCorner,
       },
       {
         face: "orange",
-        edgeOne: orangeLeftCorner,
-        edgeTwo: orangeRightCorner,
-        edgeOnePair: greenRightCorner,
-        edgeTwoPair: blueLeftCorner,
+        edgeOne: orangeTopLeftCorner,
+        edgeTwo: orangeTopRightCorner,
+        edgeOnePair: greenTopRightCorner,
+        edgeTwoPair: blueTopLeftCorner,
       },
       {
         face: "green",
-        edgeOne: greenLeftCorner,
-        edgeTwo: greenRightCorner,
-        edgeOnePair: redRightCorner,
-        edgeTwoPair: orangeLeftCorner,
+        edgeOne: greenTopLeftCorner,
+        edgeTwo: greenTopRightCorner,
+        edgeOnePair: redTopRightCorner,
+        edgeTwoPair: orangeTopLeftCorner,
       },
       {
         face: "red",
-        edgeOne: redLeftCorner,
-        edgeTwo: redRightCorner,
-        edgeOnePair: blueRightCorner,
-        edgeTwoPair: greenLeftCorner,
+        edgeOne: redTopLeftCorner,
+        edgeTwo: redTopRightCorner,
+        edgeOnePair: blueTopRightCorner,
+        edgeTwoPair: greenTopLeftCorner,
       },
     ];
 
@@ -120,39 +161,50 @@ const FirstLayerSolver = (props) => {
       if (corner.edgeOne === "white") {
         colorCornerPos = "right";
         whiteCornerPos = "left";
-        cornerEdgePair = corner.edgeOnePair;
+        cornerEdgePair = corner.edgeOnePair.toUpperCase();
         algoString = ["R", "U", "R'"];
-        case0Algo =
-          [`${corner.face.charAt(0)}`, "y", `${corner.face.charAt(0)}p`];
-        case1Algo =
-          [`${algoDirection}`,
+        case0Algo = [
+          `${corner.face.charAt(0)}`,
+          "y",
+          `${corner.face.charAt(0)}p`,
+        ];
+        case1Algo = [
+          `${algoDirection}`,
           `${rightOfEdgePairOne.charAt(0)}`,
           "y",
-          `${rightOfEdgePairOne.charAt(0)}p`];
-        case2Algo =
-          [`y`,
+          `${rightOfEdgePairOne.charAt(0)}p`,
+        ];
+        case2Algo = [
+          `y`,
           `y`,
           `${rightOfEdgePairOne.charAt(0)}`,
           "y",
-          `${rightOfEdgePairOne.charAt(0)}p`];
+          `${rightOfEdgePairOne.charAt(0)}p`,
+        ];
       }
       if (corner.edgeTwo === "white") {
         colorCornerPos = "left";
         whiteCornerPos = "right";
-        cornerEdgePair = corner.edgeTwoPair;
+        cornerEdgePair = corner.edgeTwoPair.toUpperCase();
         algoString = ["L'", "U'", "L"];
-        case0Algo = [`${corner.face.charAt(0)}p`, "yp", `${corner.face.charAt(0)}`]
-        case1Algo =
-          [`${algoDirection}`,
+        case0Algo = [
+          `${corner.face.charAt(0)}p`,
+          "yp",
+          `${corner.face.charAt(0)}`,
+        ];
+        case1Algo = [
+          `${algoDirection}`,
           `${leftOfEdgePairTwo.charAt(0)}p`,
           "yp",
-          `${leftOfEdgePairTwo.charAt(0)}`]
-        case2Algo =
-          [`y`,
+          `${leftOfEdgePairTwo.charAt(0)}`,
+        ];
+        case2Algo = [
+          `y`,
           `y`,
           `${leftOfEdgePairTwo.charAt(0)}p`,
           "yp",
-          `${leftOfEdgePairTwo.charAt(0)}`];
+          `${leftOfEdgePairTwo.charAt(0)}`,
+        ];
       }
 
       // Runs commands depending on how many moves needed
@@ -174,7 +226,7 @@ const FirstLayerSolver = (props) => {
             true,
             moveCounter,
             [
-              `The ${corner.face} face has a white edge in the top ${whiteCornerPos} corner, so match up it's adjacent color ${cornerEdgePair} to the same color center by turning the top ${move} one time!`,
+              `The ${corner.face.toUpperCase()} face has a white edge in the top ${whiteCornerPos} corner, so match up it's adjacent color ${cornerEdgePair} to the same color center by turning the top ${move} one time!`,
               `Now, facing the ${cornerEdgePair} face, perform ${algoString[0]}`,
               `Now, facing the ${cornerEdgePair} face, perform ${algoString[1]}`,
               `Now, facing the ${cornerEdgePair} face, perform ${algoString[2]}`,
@@ -187,11 +239,11 @@ const FirstLayerSolver = (props) => {
             true,
             moveCounter,
             [
-              `The ${corner.face} face has a white edge in the top ${whiteCornerPos} corner, so match up it's adjacent color ${cornerEdgePair} to the same color center by turning the top either direction twice!`,
+              `The ${corner.face.toUpperCase()} face has a white edge in the top ${whiteCornerPos} corner, so match up it's adjacent color ${cornerEdgePair} to the same color center by turning the top either direction twice!`,
               "Keep turning",
               `Now, facing the ${cornerEdgePair} face, perform ${algoString[0]}`,
-              `Now. facing the ${cornerEdgePair} face, perform ${algoString[1]}`,
-              `Now facing the ${cornerEdgePair} face, perform ${algoString[2]}`,
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[1]}`,
+              `Now, facing the ${cornerEdgePair} face, perform ${algoString[2]}`,
             ],
             case2Algo
           );
@@ -201,27 +253,108 @@ const FirstLayerSolver = (props) => {
       }
     });
   }, [
-    blueLeftCorner,
-    blueRightCorner,
-    greenLeftCorner,
-    greenRightCorner,
-    orangeLeftCorner,
-    orangeRightCorner,
-    redLeftCorner,
-    redRightCorner,
+    blueTopLeftCorner,
+    blueTopRightCorner,
+    greenTopLeftCorner,
+    greenTopRightCorner,
+    orangeTopLeftCorner,
+    orangeTopRightCorner,
+    redTopLeftCorner,
+    redTopRightCorner,
     moveCounter,
     props,
   ]);
 
+  const bottomLayer = useCallback(() => {
+    const bottomCorners = [
+      {
+        face: "blue",
+        edgeOne: blueBottomLeftCorner,
+        edgeTwo: blueBottomRightCorner,
+        leftOfFace: "orange",
+        rightOfFace: "red",
+      },
+      {
+        face: "orange",
+        edgeOne: orangeBottomLeftCorner,
+        edgeTwo: orangeBottomRightCorner,
+        leftOfFace: "green",
+        rightOfFace: "blue",
+      },
+      {
+        face: "green",
+        edgeOne: greenBottomLeftCorner,
+        edgeTwo: greenBottomRightCorner,
+        leftOfFace: "red",
+        rightOfFace: "orange",
+      },
+      {
+        face: "red",
+        edgeOne: redBottomLeftCorner,
+        edgeTwo: redBottomRightCorner,
+        leftOfFace: "blue",
+        rightOfFace: "green",
+      },
+    ];
+
+    bottomCorners.forEach((corner) => {
+      let adjacentFace;
+      let whitePos;
+      let algo;
+      let algoString2;
+      let hasWhiteEdge = false;
+      if (corner.edgeOne === "white") {
+        hasWhiteEdge = true;
+        adjacentFace = corner.leftOfFace;
+        whitePos = "left";
+        algoString2 = ["R", "U", "R'"];
+        algo = [`${corner.face.charAt(0)}`, "y", `${corner.face.charAt(0)}p`];
+      }
+      if (corner.edgeTwo === "white") {
+        hasWhiteEdge = true;
+        adjacentFace = corner.rightOfFace;
+        whitePos = "right";
+        algoString2 = ["L'", "U'", "L"];
+        algo = [`${corner.face.charAt(0)}p`, "yp", `${corner.face.charAt(0)}`];
+      }
+      if (hasWhiteEdge) {
+        props.setValuesForMultiStageCommand(
+          true,
+          moveCounter,
+          [
+            `The ${corner.face} face has a white edge in the bottom ${whitePos} corner, so facing the ${adjacentFace} face, perform ${algoString2[0]}, ${algoString2[1]}, ${algoString2[2]} in order to put the corner into the top row`,
+            `Now, facing the ${adjacentFace} face, perform ${algoString2[1]}`,
+            `Now, facing the ${adjacentFace} face, perform ${algoString2[2]}`,
+          ],
+          algo
+        );
+      }
+      hasWhiteEdge = false;
+    });
+  }, [
+    blueBottomLeftCorner,
+    blueBottomRightCorner,
+    greenBottomLeftCorner,
+    greenBottomRightCorner,
+    moveCounter,
+    orangeBottomLeftCorner,
+    orangeBottomRightCorner,
+    props,
+    redBottomLeftCorner,
+    redBottomRightCorner,
+  ]);
+
   const firstLayerSolver = useCallback(() => {
     if (!props.multiStageCommand) {
+      // checks if white corners are in the bottom layer
+      bottomLayer();
       // checks if white corners facing out in top layer
       topLayerFacingOut();
     }
     if (props.multiStageCommand) {
       props.multiStageCommandSetter();
     }
-  }, [topLayerFacingOut, props]);
+  }, [topLayerFacingOut, bottomLayer, props]);
 
   // Runs the solver if white cross not solved
   useEffect(() => {
